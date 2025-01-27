6923a0061efe7f077ecda479f530473a48371a16
faityn <faitynb@gmail.com>
2025-01-27 09:50:50 +0800

    conference delete

--- src/hooks/useEvents.ts
+++ src/hooks/useEvents.ts
@@ -843,6 +843,32 @@ export const getConferenceDetail = async (token: string, id: number) => {
   }
 };
 
+export const conferenceDeleteMulti = async (token: string, ids: string) => {
+  try {
+    const raw = JSON.stringify({
+      ids: ids,
+    });
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/admin/exhibitions/conference/delete_multy`,
+      {
+        method: "DELETE",
+        body: raw,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        redirect: "follow",
+      }
+    );
+
+    const data = await response.text();
+    return { status: response.ok, result: data };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
 export const getConferencesOptions = async (token: string) => {
   try {
     const response = await fetch(
